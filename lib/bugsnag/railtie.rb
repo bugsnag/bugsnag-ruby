6de20ca2ca8943dc048456f1b6ed09e9513878d9
Darcy Laycock <sutto@sutto.net>
2012-07-04 15:19:39 +0800

    Update master

--- lib/bugsnag/railtie.rb
+++ lib/bugsnag/railtie.rb
@@ -15,11 +15,11 @@ module Bugsnag
 
     config.after_initialize do
       Bugsnag.configure do |config|
-        config.release_stage = Rails.env
-        config.project_root = Rails.root
+        config.release_stage = ::Rails.env
+        config.project_root = ::Rails.root
         config.framework = "Rails: #{::Rails::VERSION::STRING}"
 
-        config.logger ||= Rails.logger
+        config.logger ||= ::Rails.logger
       end
 
       if defined?(::ActionController::Base)
