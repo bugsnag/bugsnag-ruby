24ce816545c4fa95d217e46509f1f8ac72fab7b7
Simon Maynard <snmaynard@gmail.com>
2012-09-29 16:58:18 -0700

    Update controller methods

--- lib/bugsnag/rails/controller_methods.rb
+++ lib/bugsnag/rails/controller_methods.rb
@@ -38,11 +38,7 @@ module Bugsnag::Rails
 
     private
     def notify_bugsnag(exception, custom_data=nil)
-      Bugsnag.warn "DEPRECATED METHOD: notify_bugsnag is deprecated and will be removed in the future. Please use Bugsnag.notify instead" if Bugsnag.configuration.release_stage != "production"
-
-      overrides = {}
-      overrides[:custom] = custom_data if custom_data
-      Bugsnag.notify(exception, overrides)
+      Bugsnag.notify(exception, custom_data)
     end
   end
 end
