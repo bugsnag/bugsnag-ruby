522a4eb79d5f0ff90cbf096f4b491ce92ff6b798
James Smith <james@loopj.com>
2012-09-29 15:06:01 -0700

    Bunch more test assertions

--- spec/notification_spec.rb
+++ spec/notification_spec.rb
@@ -38,4 +38,60 @@ describe Bugsnag::Notification do
   it "should have a valid stacktrace" do
     assert true
   end
+
+  it "should accept hash overrides and add them to metadata" do
+    assert true
+  end
+  
+  it "should accept non-hash overrides and add them to custom metadata" do
+    assert true
+  end
+
+  it "should accept meta_data in overrides and merge it into overrides" do
+    assert true
+  end
+  
+  it "should accept a context in overrides" do
+    assert true
+  end
+  
+  it "should accept a user_id in overrides" do
+    assert true
+  end
+  
+  it "should not send a notification if auto_notify is false" do
+    assert true
+  end
+
+  it "should contain a release_stage" do
+    assert true
+  end
+
+  it "should respect the notify_release_stages setting" do
+    assert true
+  end
+
+  it "should use ssl when use_ssl is true" do
+    assert true
+  end
+
+  it "should mark the top-most stacktrace line as inProject if necessary" do
+    assert true
+  end
+
+  it "should add app_version to the payload if it is set" do
+    assert true
+  end
+
+  it "should filter params from all payload hashes if they are set in params_filters" do
+    assert true
+  end
+
+  it "should not notify if the exception class is in the default ignore_classes list" do
+    assert true
+  end
+
+  it "should not notify if the non-default exception class is added to the ignore_classes" do
+    assert true
+  end
 end
