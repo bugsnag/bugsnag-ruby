9cb759b37fafa40b673d4481fdb56ad78382deb3
Simon Maynard <snmaynard@gmail.com>
2012-09-29 18:15:29 -0700

    Add some middleware tests

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -1,12 +1,90 @@
 require 'spec_helper'
-require 'securerandom'
 
 describe Bugsnag::MiddlewareStack do
-  it "should contain an api_key if one is set" do
+  it "should run before_bugsnag_notify callbacks, adding a tab" do
     Bugsnag::Notification.should_receive(:deliver_exception_payload) do |endpoint, payload|
-      payload[:apiKey].should be == "c9d60ae4c7e70c4b6c4ebd3e8056d2b8"
+      event = get_event_from_payload(payload)
+      event[:metaData][:some_tab].should_not be_nil
+      event[:metaData][:some_tab][:info].should be == "here"
+      event[:metaData][:some_tab][:data].should be == "also here"
     end
+    
+    callback_run_count = 0
+    Bugsnag.before_notify_callbacks << lambda {|notif|
+      notif.add_tab(:some_tab, {
+        :info => "here",
+        :data => "also here"
+      })
+      callback_run_count += 1
+    }
 
     Bugsnag.notify(BugsnagTestException.new("It crashed"))
+    callback_run_count.should be == 1
+  end
+  
+  it "should run before_bugsnag_notify callbacks, adding custom data" do
+    Bugsnag::Notification.should_receive(:deliver_exception_payload) do |endpoint, payload|
+      event = get_event_from_payload(payload)
+      event[:metaData][:custom].should_not be_nil
+      event[:metaData][:custom][:info].should be == "here"
+      event[:metaData][:custom][:data].should be == "also here"
+    end
+    
+    callback_run_count = 0
+    Bugsnag.before_notify_callbacks << lambda {|notif|
+      notif.add_custom_data(:info, "here")
+      notif.add_custom_data(:data, "also here")
+      
+      callback_run_count += 1
+    }
+
+    Bugsnag.notify(BugsnagTestException.new("It crashed"))
+    callback_run_count.should be == 1
+  end
+  
+  it "overrides should override data set in before_notify" do
+    Bugsnag::Notification.should_receive(:deliver_exception_payload) do |endpoint, payload|
+      event = get_event_from_payload(payload)
+      event[:metaData][:custom].should_not be_nil
+      event[:metaData][:custom][:info].should be == "here2"
+      event[:metaData][:custom][:data].should be == "also here"
+    end
+    
+    callback_run_count = 0
+    Bugsnag.before_notify_callbacks << lambda {|notif|
+      notif.add_custom_data(:info, "here")
+      notif.add_custom_data(:data, "also here")
+      
+      callback_run_count += 1
+    }
+
+    Bugsnag.notify(BugsnagTestException.new("It crashed"), {:info => "here2"})
+    callback_run_count.should be == 1
+  end
+  
+  it "should have no before or after callbacks by default" do
+    Bugsnag::Notification.should_receive(:deliver_exception_payload) do |endpoint, payload|
+      event = get_event_from_payload(payload)
+      event[:metaData].should have(0).items
+    end
+    
+    Bugsnag.before_notify_callbacks.should have(0).items
+    Bugsnag.after_notify_callbacks.should have(0).items
+    Bugsnag.notify(BugsnagTestException.new("It crashed"))
+  end
+  
+  it "should run after_bugsnag_notify callbacks" do
+    Bugsnag::Notification.should_receive(:deliver_exception_payload) do |endpoint, payload|
+      event = get_event_from_payload(payload)
+    end
+    
+    callback_run_count = 0
+    Bugsnag.after_notify_callbacks << lambda {|notif|
+      callback_run_count += 1
+    }
+
+    Bugsnag.notify(BugsnagTestException.new("It crashed"))
+    
+    callback_run_count.should be == 1
   end
 end
