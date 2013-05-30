4986cbb9bdc63a25c206ebb896da5ef8d3a58077
Simon Maynard <snmaynard@gmail.com>
2013-05-30 13:13:58 +0100

    Working

--- lib/bugsnag/helpers.rb
+++ lib/bugsnag/helpers.rb
@@ -32,6 +32,14 @@ module Bugsnag
         obj.to_s unless obj.to_s =~ /#<.*>/
       end
     end
+
+    def self.cleanup_url(url, filters = nil)
+      return url unless filters && filters.is_a?(Array)
+
+      filter_regex = Regexp.new("([?&](?:[^&=]*#{filters.join('|[^&=]*')}[^&=]*)=)[^&]*")
+      
+      url.gsub(filter_regex, '\1[FILTERED]')
+    end
     
     def self.reduce_hash_size(hash)
       return {} unless hash.is_a?(Hash)
