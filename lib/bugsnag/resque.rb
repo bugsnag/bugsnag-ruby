6a06158761029688e72c607240fe72e25a5e3dbe
James Smith <james@loopj.com>
2013-10-09 18:58:13 -0700

    Auto-load bugsnag failure backend when using resque

---
+++ lib/bugsnag/resque.rb
@@ -0,0 +1,37 @@
+require "resque/failure/base"
+
+module Bugsnag
+  class Resque < ::Resque::Failure::Base
+    def self.configure(&block)
+      add_failure_backend
+      ::Bugsnag.configure(&block)
+    end
+
+    def self.add_failure_backend
+      return if ::Resque::Failure.backend == self
+
+      # Ensure resque is using a "Multiple" failure backend
+      unless ::Resque::Failure.backend < ::Resque::Failure::Multiple
+        original_backend = ::Resque::Failure.backend
+        ::Resque::Failure.backend = ::Resque::Failure::Multiple
+        ::Resque::Failure.backend.classes ||= []
+        ::Resque::Failure.backend.classes << original_backend
+      end
+
+      # Add Bugsnag failure backend
+      unless ::Resque::Failure.backend.classes.include?(self)
+        ::Resque::Failure.backend.classes << self
+      end
+    end
+
+    def save
+      ::Bugsnag.auto_notify(exception, {:context => "resque##{queue}", :payload => payload})
+    end
+  end
+end
+
+# For backwards compatibility
+Resque::Failure::Bugsnag = Bugsnag::Resque
+
+# Auto-load the failure backend
+Bugsnag::Resque.add_failure_backend
