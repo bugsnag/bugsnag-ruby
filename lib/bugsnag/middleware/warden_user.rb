99cb00aedb5827a3de74fa619006e0e03a0b2dfa
Simon Maynard <snmaynard@gmail.com>
2013-12-17 13:45:42 -0800

    Simplify user tab

--- lib/bugsnag/middleware/warden_user.rb
+++ lib/bugsnag/middleware/warden_user.rb
@@ -1,7 +1,7 @@
 module Bugsnag::Middleware
   class WardenUser
     SCOPE_PATTERN = /^warden\.user\.([^.]+)\.key$/
-    COMMON_USER_FIELDS = [:email, :name, :first_name, :last_name, :created_at]
+    COMMON_USER_FIELDS = [:email, :name, :first_name, :last_name, :created_at, :id]
 
     def initialize(bugsnag)
       @bugsnag = bugsnag
@@ -18,34 +18,18 @@ module Bugsnag::Middleware
           # Pick the best scope for unique id (the default is "user")
           best_scope = warden_scopes.include?("user") ? "user" : warden_scopes.first
 
-          # Set the user_id
-          if best_scope
-            scope_key = "warden.user.#{best_scope}.key"
-            scope = session[scope_key]
-            if scope.is_a? Array
-              user_ids = scope.detect {|el| el.is_a? Array}
-              if user_ids
-                user_id = user_ids.first
-                notification.user_id = user_id unless user_id.nil?
-              end
-            end
-          end
-
           # Extract useful user information
-          warden_tab = {}
-          warden_scopes.each do |scope|
-            user_object = env["warden"].user({:scope => scope, :run_callbacks => false}) rescue nil
-            if user_object
-              # Build the user info for this scope
-              scope_hash = warden_tab["Warden #{scope.capitalize}"] = {}
-              COMMON_USER_FIELDS.each do |field|
-                scope_hash[field] = user_object.send(field) if user_object.respond_to?(field)
-              end
+          user = {}
+          user_object = env["warden"].user({:scope => best_scope, :run_callbacks => false}) rescue nil
+          if user_object
+            # Build the user info for this scope
+            COMMON_USER_FIELDS.each do |field|
+              user[field] = user_object.send(field) if user_object.respond_to?(field)
             end
           end
 
           # We merge the first warden scope down, so that it is the main "user" for the request
-          notification.user = warden_tab.merge(warden_tab.first[1]) unless warden_tab.empty?
+          notification.user = user unless user.empty?
         end
       end
 
