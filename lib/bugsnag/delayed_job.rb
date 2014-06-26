64bf3e92940b69ca6ff73008d6e6b9604e67465b
dtaniwaki <daisuketaniwaki@gmail.com>
2014-06-26 09:27:49 +0900

    Add job detail for delayed_job

--- lib/bugsnag/delayed_job.rb
+++ lib/bugsnag/delayed_job.rb
@@ -13,7 +13,31 @@ unless defined? Delayed::Plugins::Bugsnag
       class Bugsnag < Plugin
         module Notify
           def error(job, error)
-            ::Bugsnag.auto_notify(error)
+            overrides = {
+              :job => {
+                :class => job.class.name,
+                :id => job.id,
+              }
+            }
+            if payload = job.payload_object
+              p = {
+                :class => payload.class.name,
+              }
+              p[:id]           = payload.id           if payload.respond_to?(:id)
+              p[:display_name] = payload.display_name if payload.respond_to?(:display_name)
+              p[:method_name]  = payload.method_name  if payload.respond_to?(:method_name)
+              p[:args]         = payload.args         if payload.respond_to?(:args)
+              if payload.is_a?(::Delayed::PerformableMethod) && (object = payload.object)
+                p[:object] = {
+                  :class => object.class.name,
+                }
+                p[:object][:id] = object.id if object.respond_to?(:id)
+              end
+              overrides[:job][:payload] = p
+            end
+
+            ::Bugsnag.auto_notify(error, overrides)
+
             super if defined?(super)
           end
         end
