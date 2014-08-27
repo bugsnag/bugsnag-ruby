6a4786f7fb6a6f11c1fd035a2f2888f5fb9fa406
Kyrylo Silin <kyrylosilin@gmail.com>
2014-08-26 18:32:16 -0700

    lib/bugsnag/middleware/rack_request: add the comments back

--- lib/bugsnag/middleware/rack_request.rb
+++ lib/bugsnag/middleware/rack_request.rb
@@ -17,6 +17,7 @@ module Bugsnag::Middleware
         # @see https://github.com/rails/rails/issues/10813
         session["session_id"]
 
+        # Set the context
         notification.context = "#{request.request_method} #{request.path}"
 
         # Set a sensible default for user_id
@@ -27,6 +28,7 @@ module Bugsnag::Middleware
         url << ":#{request.port}" unless [80, 443].include?(request.port)
         url << Bugsnag::Helpers.cleanup_url(request.fullpath, notification.configuration.params_filters)
 
+        # Add a request tab
         notification.add_tab(:request, {
           :url => url,
           :httpMethod => request.request_method,
@@ -36,8 +38,10 @@ module Bugsnag::Middleware
           :clientIp => request.ip
         })
 
+        # Add an environment tab
         notification.add_tab(:environment, env)
 
+        # Add a session tab
         if session
           # Rails 3
           if session.is_a?(Hash)
@@ -48,6 +52,7 @@ module Bugsnag::Middleware
           end
         end
 
+        # Add a cookies tab
         cookies = request.cookies
         notification.add_tab(:cookies, cookies) if cookies
       end
