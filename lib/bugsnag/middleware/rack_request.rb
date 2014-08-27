5ca9865c5ca49936c9264cc28f8cc2a55af94d5d
Kyrylo Silin <kyrylosilin@gmail.com>
2014-08-26 17:39:33 -0700

    lib/bugsnag/middleware/rack_request: early load session for Rails 4
    https://github.com/rails/rails/issues/10813

--- lib/bugsnag/middleware/rack_request.rb
+++ lib/bugsnag/middleware/rack_request.rb
@@ -3,16 +3,19 @@ module Bugsnag::Middleware
     def initialize(bugsnag)
       @bugsnag = bugsnag
     end
-      
+
     def call(notification)
       if notification.request_data[:rack_env]
         env = notification.request_data[:rack_env]
 
         request = ::Rack::Request.new(env)
         params = request.params
-        session = env["rack.session"]
 
-        # Set the context
+        session = env["rack.session"]
+        # Load session for Rails (for Rails 4 it's lazy loaded).
+        # @see https://github.com/rails/rails/issues/10813
+        session["session_id"]
+
         notification.context = "#{request.request_method} #{request.path}"
 
         # Set a sensible default for user_id
@@ -23,7 +26,6 @@ module Bugsnag::Middleware
         url << ":#{request.port}" unless [80, 443].include?(request.port)
         url << Bugsnag::Helpers.cleanup_url(request.fullpath, notification.configuration.params_filters)
 
-        # Add a request tab
         notification.add_tab(:request, {
           :url => url,
           :httpMethod => request.request_method,
@@ -32,14 +34,19 @@ module Bugsnag::Middleware
           :referer => request.referer,
           :clientIp => request.ip
         })
-          
-        # Add an environment tab
+
         notification.add_tab(:environment, env)
 
-        # Add a session tab
-        notification.add_tab(:session, session) if session
+        if session
+          # Rails 3
+          if session.is_a?(Hash)
+            notification.add_tab(:session, session)
+          else
+            # Rails 4
+            notification.add_tab(:session, session.to_hash)
+          end
+        end
 
-        # Add a cookies tab
         cookies = request.cookies
         notification.add_tab(:cookies, cookies) if cookies
       end
