f72d8469029b8ab606359aa59e26306fb23ef21e
Kyrylo Silin <kyrylosilin@gmail.com>
2014-08-26 18:05:41 -0700

    lib/bugsnag/middleware/rack_request: early load session for Rails 4
    Fixes #144 (Rails 4 sessions appear in custom tab)
    
    Context: https://github.com/rails/rails/issues/10813

--- lib/bugsnag/middleware/rack_request.rb
+++ lib/bugsnag/middleware/rack_request.rb
@@ -13,8 +13,10 @@ module Bugsnag::Middleware
         params = request.params rescue {}
 
         session = env["rack.session"]
+        # Load session for Rails (for Rails 4 it's lazy loaded).
+        # @see https://github.com/rails/rails/issues/10813
+        session["session_id"]
 
-        # Set the context
         notification.context = "#{request.request_method} #{request.path}"
 
         # Set a sensible default for user_id
@@ -25,7 +27,6 @@ module Bugsnag::Middleware
         url << ":#{request.port}" unless [80, 443].include?(request.port)
         url << Bugsnag::Helpers.cleanup_url(request.fullpath, notification.configuration.params_filters)
 
-        # Add a request tab
         notification.add_tab(:request, {
           :url => url,
           :httpMethod => request.request_method,
@@ -35,13 +36,18 @@ module Bugsnag::Middleware
           :clientIp => request.ip
         })
 
-        # Add an environment tab
         notification.add_tab(:environment, env)
 
-        # Add a session tab
-        notification.add_tab(:session, session) if session
+        if session
+          # Rails 3
+          if session.is_a?(Hash)
+            notification.add_tab(:session, session)
+          else
+            # Rails 4
+            notification.add_tab(:session, session.to_hash)
+          end
+        end
 
-        # Add a cookies tab
         cookies = request.cookies
         notification.add_tab(:cookies, cookies) if cookies
       end
