e1d33e2ef2a1b0fd72680784dfd44507ab83dd8f
Keegan Lowenstein <keegan@bugsnag.com>
2015-02-17 16:40:25 -0800

    Move railtie params_filters configuration to after_initialize
    Fixes #164

--- lib/bugsnag/railtie.rb
+++ lib/bugsnag/railtie.rb
@@ -15,24 +15,16 @@ module Bugsnag
     config.before_initialize do
       # Configure bugsnag rails defaults
       Bugsnag.configure do |config|
-        config.logger = Rails.logger
-        config.release_stage = Rails.env.to_s
-        config.project_root = Rails.root.to_s
-        config.params_filters += Rails.configuration.filter_parameters.map do |filter|
-          case filter
-          when String, Symbol
-            /\A#{filter}\z/
-          else
-            filter
-          end
-        end
+        config.logger = ::Rails.logger
+        config.release_stage = ::Rails.env.to_s
+        config.project_root = ::Rails.root.to_s
         config.middleware.insert_before Bugsnag::Middleware::Callbacks, Bugsnag::Middleware::Rails3Request
       end
 
       # Auto-load configuration settings from config/bugsnag.yml if it exists
-      config_file = Rails.root.join("config", "bugsnag.yml")
+      config_file = ::Rails.root.join("config", "bugsnag.yml")
       config = YAML.load_file(config_file) if File.exists?(config_file)
-      Bugsnag.configure(config[Rails.env] ? config[Rails.env] : config) if config
+      Bugsnag.configure(config[::Rails.env] ? config[::Rails.env] : config) if config
 
       if defined?(::ActionController::Base)
         require "bugsnag/rails/controller_methods"
@@ -41,6 +33,21 @@ module Bugsnag
       if defined?(ActiveRecord::Base)
         require "bugsnag/rails/active_record_rescue"
         ActiveRecord::Base.send(:include, Bugsnag::Rails::ActiveRecordRescue)
+      end
+    end
+
+    # Configure params_filters after initialization, so that rails initializers
+    # may set filter_parameters which will be picked up by Bugsnag.
+    config.after_initialize do
+      Bugsnag.configure do |config|
+        config.params_filters += ::Rails.configuration.filter_parameters.map do |filter|
+          case filter
+          when String, Symbol
+            /\A#{filter}\z/
+          else
+            filter
+          end
+        end
       end
     end
 
