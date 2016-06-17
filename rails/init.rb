130ba926e7b051b98926b12cd6e4bcd7b5cb732c
Delisa Mason <iskanamagus@gmail.com>
2016-06-17 15:12:02 -0700

    Ensure Rails 2 legacy extensions are not loaded on newer versions
    #303

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,7 @@
 # On Rails 2.x GEM_ROOT/rails/init.rb is auto loaded for all gems
 # so this is the place to initialize Rails 2.x plugin support
-require "bugsnag/rails"
+if Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new("3.0")
+  require "bugsnag/rails"
+else
+  Bugsnag.warn "Blocked attempt to initialize legacy Rails 2.x extensions"
+end
