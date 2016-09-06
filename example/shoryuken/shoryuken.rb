3e9d17a96d35d29a3c4df8c89ec30e5618130aad
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2016-09-06 21:35:43 +1200

    Bugsnag integration with Shoryuken, including working example

---
+++ example/shoryuken/shoryuken.rb
@@ -0,0 +1,41 @@
+require 'shoryuken'
+require 'bugsnag'
+
+Bugsnag.configure do |config|
+  config.api_key = '066f5ad3590596f9aa8d601ea89af845'
+end
+
+Shoryuken.configure_server do |config|
+  if defined?(Rails)
+    # Replace Rails logger so messages are logged wherever Shoryuken is logging
+    # Note: this entire block is only run by the processor, so we don't overwrite
+    #       the logger when the app is running as usual.
+    Rails.logger = Shoryuken::Logging.logger
+  end
+end
+
+class PlainOldRuby
+  include Shoryuken::Worker
+
+  shoryuken_options queue: 'connector_development_default', auto_delete: true
+
+  def perform(sqs_msg, body)
+    puts "Received message: #{body}"
+    raise 'Uh oh!'
+  end
+end
+
+# Start up shoryuken processor via:
+#
+# bundle exec shoryuken -r ./shoryuken.rb -C ./shoryuken.yml
+
+# Then you can open up console session like so:
+#
+# export AWS_ACCESS_KEY_ID=ABCDEFG123456789
+# export AWS_SECRET_ACCESS_KEY=9876543210ZYX
+# export AWS_REGION=ap-southeast-2
+#
+# irb -r ./shoryuken.rb
+#
+# where you can then say
+# PlainOldRuby.perform_async "hello"
---
+++ lib/bugsnag/shoryuken.rb
@@ -0,0 +1,34 @@
+require 'shoryuken'
+
+module Bugsnag
+  class Shoryuken
+    def initialize
+      Bugsnag.configuration.app_type = "shoryuken"
+      Bugsnag.configuration.delivery_method = :synchronous
+    end
+
+    def call(_, queue, _, body)
+      begin
+        Bugsnag.before_notify_callbacks << lambda {|notification|
+          notification.add_tab(:shoryuken, {
+            queue: queue,
+            body: body
+          })
+        }
+
+        yield
+      rescue Exception => ex
+        Bugsnag.auto_notify(ex) unless [Interrupt, SystemExit, SignalException].include?(ex.class)
+        raise
+      ensure
+        Bugsnag.clear_request_data
+      end
+    end
+  end
+end
+
+::Shoryuken.configure_server do |config|
+  config.server_middleware do |chain|
+    chain.add ::Bugsnag::Shoryuken
+  end
+end
