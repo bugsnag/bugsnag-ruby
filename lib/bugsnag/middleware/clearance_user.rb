7bbf716dab63b2772e441c3bd1014a1940551a19
Keegan Lowenstein <keegan@bugsnag.com>
2016-11-11 18:05:56 -0800

    use symbol for clearance rack_env key

--- lib/bugsnag/middleware/clearance_user.rb
+++ lib/bugsnag/middleware/clearance_user.rb
@@ -8,13 +8,13 @@ module Bugsnag::Middleware
 
     def call(notification)
       if notification.request_data[:rack_env] &&
-            notification.request_data[:rack_env]["clearance"] &&
-            notification.request_data[:rack_env]["clearance"].signed_in? &&
-            notification.request_data[:rack_env]["clearance"].current_user
+            notification.request_data[:rack_env][:clearance] &&
+            notification.request_data[:rack_env][:clearance].signed_in? &&
+            notification.request_data[:rack_env][:clearance].current_user
 
         # Extract useful user information
         user = {}
-        user_object = notification.request_data[:rack_env]["clearance"].current_user
+        user_object = notification.request_data[:rack_env][:clearance].current_user
         if user_object
           # Build the bugsnag user info from the current user record
           COMMON_USER_FIELDS.each do |field|
