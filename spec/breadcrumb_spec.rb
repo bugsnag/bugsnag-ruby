2fc35f051f98a5032d830745fa3ccb5ce48ea710
Alex Moinet <alex@bugsnag.com>
2017-09-10 16:01:01 +0100

    Updated tests for breadcrumb updates

--- spec/breadcrumb_spec.rb
+++ spec/breadcrumb_spec.rb
@@ -3,74 +3,49 @@ require 'spec_helper'
 
 describe Bugsnag::Breadcrumbs::Breadcrumb do
     context "when creating a breadcrumb" do
-        it "should throw an ArgumentError if no name is given" do
-            begin
-                Bugsnag::Breadcrumbs::Breadcrumb.new
-            rescue => exception
-                expect(exception).to be_a_kind_of(ArgumentError)
-            end
+
+        it "should trim the name if the name is too long" do
+            name = "x" * 50
+            crumb = Bugsnag::Breadcrumbs::Breadcrumb.new(name)
+            expect(crumb.name).to eq("x" * 30)
         end
 
-        it "should throw an ArgumentError if the name is an empty string" do
-            begin
-                Bugsnag::Breadcrumbs::Breadcrumb.new("", nil)
-            rescue => exception
-                expect(exception).to be_a_kind_of(ArgumentError)
-            end
+        it "should default to a MANUAL_TYPE if no type given" do
+            crumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test")
+            expect(crumb.type).to eq(Bugsnag::Breadcrumbs::MANUAL_TYPE)
         end
 
-        it "should throw an ArgumentError if the name is too long" do
-            begin
-                name = "x" * 50
-                Bugsnag::Breadcrumbs::Breadcrumb.new(name, nil)
-            rescue => exception
-                expect(exception).to be_a_kind_of(ArgumentError)
-            end
-        end
-
-        it "should throw an ArgumentError if no type given" do
-            begin
-                Bugsnag::Breadcrumbs::Breadcrumb.new("Test", nil)
-            rescue => exception
-                expect(exception).to be_a_kind_of(ArgumentError)
-            end       
-        end
-
-        it "should throw an ArgumentError if an invalid type is given" do
-            begin
-                Bugsnag::Breadcrumbs::Breadcrumb.new("Test", "Error")
-            rescue => exception
-                expect(exception).to be_a_kind_of(ArgumentError)
-            end
+        it "should default to a MANUAL_TYPE if an invalid type is given" do
+            crumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", "Error")
+            expect(crumb.type).to eq(Bugsnag::Breadcrumbs::MANUAL_TYPE)
         end
 
         it "should assign the name and type specified" do
-            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::MANUAL_TYPE)
+            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE)
             expect(breadcrumb.name).to eq("Test")
-            expect(breadcrumb.type).to eq(Bugsnag::Breadcrumbs::MANUAL_TYPE)
+            expect(breadcrumb.type).to eq(Bugsnag::Breadcrumbs::ERROR_TYPE)
             expect(breadcrumb.metadata).to eq({})
         end
 
         it "should create a timestamp" do
-            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::MANUAL_TYPE)
+            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE)
             expect(breadcrumb.timestamp).to be_a_kind_of(String)
         end
 
         it "should assign the metadata if specified" do
-            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::MANUAL_TYPE, {:foo => "foo", :bar => "bar"})
+            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE, {:foo => "foo", :bar => "bar"})
             expect(breadcrumb.metadata).to include(:foo => "foo")
             expect(breadcrumb.metadata).to include(:bar => "bar")
         end
     end
 
-    context  "when calling as_hash" do
+    context  "when calling as_json" do
         it "should return a hash" do
-            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::MANUAL_TYPE, {:foo => "foo", :bar => "bar"})
-            expect(breadcrumb.as_hash).to be_a_kind_of(Hash)
-            expect(breadcrumb.as_hash).to include(:name => "Test")
-            expect(breadcrumb.as_hash).to include(:type => Bugsnag::Breadcrumbs::MANUAL_TYPE)
-            expect(breadcrumb.as_hash).to include(:timestamp => be_a_kind_of(String))
-            expect(breadcrumb.as_hash).to_not include(:metadata)
+            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE, {:foo => "foo", :bar => "bar"})
+            expect(breadcrumb.as_json).to include(:name => "Test")
+            expect(breadcrumb.as_json).to include(:type => Bugsnag::Breadcrumbs::ERROR_TYPE)
+            expect(breadcrumb.as_json).to include(:timestamp => be_a_kind_of(String))
+            expect(breadcrumb.as_json).to include(:metaData => {:foo => "foo", :bar => "bar"})
         end
     end
 end
--- spec/recorder_spec.rb
+++ spec/recorder_spec.rb
@@ -3,19 +3,6 @@ require 'spec_helper'
 
 describe Bugsnag::Breadcrumbs::Recorder do
 
-    context "when creating" do
-        it "does not initialize any breadcrumbs" do
-            recorder = Bugsnag::Breadcrumbs::Recorder.new
-            expect(recorder.breadcrumbs).to be_a_kind_of(Array)
-            expect(recorder.breadcrumbs.length).to eq(0)
-        end
-
-        it "intializes counter at 0" do
-            recorder = Bugsnag::Breadcrumbs::Recorder.new
-            expect(recorder.current_item).to eq(0)
-        end
-    end
-
     context "when adding breadcrumbs" do
         before do
             @recorder = Bugsnag::Breadcrumbs::Recorder.new
@@ -23,16 +10,8 @@ describe Bugsnag::Breadcrumbs::Recorder do
 
         it "stores the breadcrumb" do
             @recorder.add_breadcrumb "Test"
-            expect(@recorder.breadcrumbs.length).to eq(1)
-            expect(@recorder.breadcrumbs).to include("Test")
-        end
-
-        it "increments the counter" do
-            @recorder.add_breadcrumb 0
-            expect(@recorder.current_item).to eq(1)
-
-            @recorder.add_breadcrumb 1
-            expect(@recorder.current_item).to eq(2)
+            expect(@recorder.length).to eq(1)
+            expect(@recorder).to include("Test")
         end
 
         it "does not store more than MAX_ITEMS breadcrumbs" do
@@ -40,7 +19,7 @@ describe Bugsnag::Breadcrumbs::Recorder do
             [*1..(max_items + 10)].each do |crumb|
                 @recorder.add_breadcrumb crumb
             end
-            expect(@recorder.breadcrumbs.length).to be <= max_items
+            expect(@recorder.length).to be <= max_items
         end
 
         it "wraps the breadcrumbs" do
@@ -49,10 +28,10 @@ describe Bugsnag::Breadcrumbs::Recorder do
                 @recorder.add_breadcrumb crumb
             end
             [*11..(max_items + 10)].each do |val|
-                expect(@recorder.breadcrumbs).to include(val)
+                expect(@recorder).to include(val)
             end
             [*1..10].each do |val|
-                expect(@recorder.breadcrumbs).to_not include(val)
+                expect(@recorder).to_not include(val)
             end
         end
     end
@@ -62,21 +41,13 @@ describe Bugsnag::Breadcrumbs::Recorder do
             @recorder = Bugsnag::Breadcrumbs::Recorder.new
         end
 
-        it "throws an error if no block_given" do
-            begin
-                @recorder.get_breadcrumbs
-            rescue => exception
-                expect(exception).to be_a_kind_of(ArgumentError)
-            end
-        end
-
         it "yields breadcrumbs in order" do
             input = [*1..10]
             input.each do |crumb|
                 @recorder.add_breadcrumb crumb
             end
             output = []
-            @recorder.get_breadcrumbs do |crumb|
+            @recorder.each do |crumb|
                 output.push(crumb)
             end
             expect(output).to eq(input)
@@ -88,7 +59,7 @@ describe Bugsnag::Breadcrumbs::Recorder do
                 @recorder.add_breadcrumb crumb
             end
             output = []
-            @recorder.get_breadcrumbs do |crumb|
+            @recorder.each do |crumb|
                 output.push(crumb)
             end
             expect(output).to eq([*11..(max_items + 10)])
