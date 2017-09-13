d0154ae8de2988623368f8435f72e216dea15aa2
Alex Moinet <alex@bugsnag.com>
2017-09-13 11:17:16 +0100

    Updated four to two space indents

--- lib/bugsnag/breadcrumbs/breadcrumb.rb
+++ lib/bugsnag/breadcrumbs/breadcrumb.rb
@@ -1,55 +1,55 @@
 module Bugsnag
-    module Breadcrumbs
+  module Breadcrumbs
 
-        MAX_SIZE = 4096
-        NAME_SIZE_LIMIT = 30
-        NAVIGATION_TYPE = "navigation"
-        REQUEST_TYPE = "request"
-        PROCESS_TYPE = "process"
-        LOG_TYPE = "log"
-        USER_TYPE = "user"
-        STATE_TYPE = "state"
-        ERROR_TYPE = "error"
-        MANUAL_TYPE = "manual"
+    MAX_SIZE = 4096
+    NAME_SIZE_LIMIT = 30
+    NAVIGATION_TYPE = "navigation"
+    REQUEST_TYPE = "request"
+    PROCESS_TYPE = "process"
+    LOG_TYPE = "log"
+    USER_TYPE = "user"
+    STATE_TYPE = "state"
+    ERROR_TYPE = "error"
+    MANUAL_TYPE = "manual"
 
-        VALID_TYPES = [
-            NAVIGATION_TYPE,
-            REQUEST_TYPE,
-            PROCESS_TYPE,
-            LOG_TYPE,
-            USER_TYPE,
-            STATE_TYPE,
-            ERROR_TYPE,
-            MANUAL_TYPE
-        ]
+    VALID_TYPES = [
+        NAVIGATION_TYPE,
+        REQUEST_TYPE,
+        PROCESS_TYPE,
+        LOG_TYPE,
+        USER_TYPE,
+        STATE_TYPE,
+        ERROR_TYPE,
+        MANUAL_TYPE
+    ]
 
-        class Breadcrumb
+    class Breadcrumb
 
-            attr_accessor :name
-            attr_accessor :type
-            attr_accessor :timestamp
-            attr_accessor :metadata
+      attr_accessor :name
+      attr_accessor :type
+      attr_accessor :timestamp
+      attr_accessor :metadata
 
-            def initialize(name, type=nil, metadata = {})
-                self.timestamp = Time.now.utc.strftime "%Y-%m-%dT%H:%MZ"
+      def initialize(name, type=nil, metadata = {})
+        self.timestamp = Time.now.utc.strftime "%Y-%m-%dT%H:%MZ"
 
-                name = name.to_s.slice(0, Bugsnag::Breadcrumbs::NAME_SIZE_LIMIT) 
-                type = Breadcrumbs::MANUAL_TYPE unless Breadcrumbs::VALID_TYPES.include? type
+        name = name.to_s.slice(0, Bugsnag::Breadcrumbs::NAME_SIZE_LIMIT) 
+        type = Breadcrumbs::MANUAL_TYPE unless Breadcrumbs::VALID_TYPES.include? type
 
-                self.name = name
-                self.type = type
-                self.metadata = metadata
-            end
+        self.name = name
+        self.type = type
+        self.metadata = metadata
+      end
 
-            def as_json
-                hash = {
-                    :timestamp => timestamp,
-                    :name => name,
-                    :type => type,
-                }
-                hash[:metaData] = metadata unless JSON::dump(metadata).length > Bugsnag::Breadcrumbs::MAX_SIZE
-                hash
-            end
-        end
+      def as_json
+        hash = {
+          :timestamp => timestamp,
+          :name => name,
+          :type => type,
+        }
+        hash[:metaData] = metadata unless JSON::dump(metadata).length > Bugsnag::Breadcrumbs::MAX_SIZE
+        hash
+      end
     end
+  end
 end
--- spec/breadcrumb_spec.rb
+++ spec/breadcrumb_spec.rb
@@ -2,50 +2,50 @@
 require 'spec_helper'
 
 describe Bugsnag::Breadcrumbs::Breadcrumb do
-    context "when creating a breadcrumb" do
+  context "when creating a breadcrumb" do
 
-        it "should trim the name if the name is too long" do
-            name = "x" * 50
-            crumb = Bugsnag::Breadcrumbs::Breadcrumb.new(name)
-            expect(crumb.name).to eq("x" * 30)
-        end
-
-        it "should default to a MANUAL_TYPE if no type given" do
-            crumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test")
-            expect(crumb.type).to eq(Bugsnag::Breadcrumbs::MANUAL_TYPE)
-        end
-
-        it "should default to a MANUAL_TYPE if an invalid type is given" do
-            crumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", "Error")
-            expect(crumb.type).to eq(Bugsnag::Breadcrumbs::MANUAL_TYPE)
-        end
-
-        it "should assign the name and type specified" do
-            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE)
-            expect(breadcrumb.name).to eq("Test")
-            expect(breadcrumb.type).to eq(Bugsnag::Breadcrumbs::ERROR_TYPE)
-            expect(breadcrumb.metadata).to eq({})
-        end
-
-        it "should create a timestamp" do
-            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE)
-            expect(breadcrumb.timestamp).to be_a_kind_of(String)
-        end
-
-        it "should assign the metadata if specified" do
-            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE, {:foo => "foo", :bar => "bar"})
-            expect(breadcrumb.metadata).to include(:foo => "foo")
-            expect(breadcrumb.metadata).to include(:bar => "bar")
-        end
+    it "should trim the name if the name is too long" do
+      name = "x" * 50
+      crumb = Bugsnag::Breadcrumbs::Breadcrumb.new(name)
+      expect(crumb.name).to eq("x" * 30)
     end
 
-    context  "when calling as_json" do
-        it "should return a hash" do
-            breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE, {:foo => "foo", :bar => "bar"})
-            expect(breadcrumb.as_json).to include(:name => "Test")
-            expect(breadcrumb.as_json).to include(:type => Bugsnag::Breadcrumbs::ERROR_TYPE)
-            expect(breadcrumb.as_json).to include(:timestamp => be_a_kind_of(String))
-            expect(breadcrumb.as_json).to include(:metaData => {:foo => "foo", :bar => "bar"})
-        end
+    it "should default to a MANUAL_TYPE if no type given" do
+      crumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test")
+      expect(crumb.type).to eq(Bugsnag::Breadcrumbs::MANUAL_TYPE)
     end
+
+    it "should default to a MANUAL_TYPE if an invalid type is given" do
+      crumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", "Error")
+      expect(crumb.type).to eq(Bugsnag::Breadcrumbs::MANUAL_TYPE)
+    end
+
+    it "should assign the name and type specified" do
+      breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE)
+      expect(breadcrumb.name).to eq("Test")
+      expect(breadcrumb.type).to eq(Bugsnag::Breadcrumbs::ERROR_TYPE)
+      expect(breadcrumb.metadata).to eq({})
+    end
+
+    it "should create a timestamp" do
+      breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE)
+      expect(breadcrumb.timestamp).to be_a_kind_of(String)
+    end
+
+    it "should assign the metadata if specified" do
+      breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE, {:foo => "foo", :bar => "bar"})
+      expect(breadcrumb.metadata).to include(:foo => "foo")
+      expect(breadcrumb.metadata).to include(:bar => "bar")
+    end
+  end
+
+  context  "when calling as_json" do
+    it "should return a hash" do
+      breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new("Test", Bugsnag::Breadcrumbs::ERROR_TYPE, {:foo => "foo", :bar => "bar"})
+      expect(breadcrumb.as_json).to include(:name => "Test")
+      expect(breadcrumb.as_json).to include(:type => Bugsnag::Breadcrumbs::ERROR_TYPE)
+      expect(breadcrumb.as_json).to include(:timestamp => be_a_kind_of(String))
+      expect(breadcrumb.as_json).to include(:metaData => {:foo => "foo", :bar => "bar"})
+    end
+  end
 end
