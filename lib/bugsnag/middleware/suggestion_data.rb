399c2d4736adb142f555d0de54d191aae62dabce
Alex Moinet <alex@bugsnag.com>
2017-09-25 15:07:53 +0100

    Spelling and references

--- lib/bugsnag/middleware/suggestion_data.rb
+++ lib/bugsnag/middleware/suggestion_data.rb
@@ -2,7 +2,7 @@ module Bugsnag::Middleware
   class SuggestionData
 
     CAPTURE_REGEX = /Did you mean\?([\s\S]+)$/
-    DELIMITTER = "\n"
+    DELIMITER = "\n"
 
     def initialize(bugsnag)
       @bugsnag = bugsnag
@@ -14,12 +14,12 @@ module Bugsnag::Middleware
         match = CAPTURE_REGEX.match(exception.message)
         next unless match
         
-        suggestions = match.captures[0].split(DELIMITTER).each { |suggestion|
+        suggestions = match.captures[0].split(DELIMITER).each { |suggestion|
           matches[matches.size] = suggestion.strip
         }
       end
 
-      report.add_tab(:"did you mean", matches) if matches.size > 0
+      report.add_tab(:suggestion, matches) if matches.size > 0
       
       @bugsnag.call(report)
     end
