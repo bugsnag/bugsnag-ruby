0f902f93943b3b22df3158e24f111e46ca971101
Alex Moinet <alex@bugsnag.com>
2017-11-17 14:52:32 +0000

    Symbolised array keys

--- lib/bugsnag/middleware/clearance_user.rb
+++ lib/bugsnag/middleware/clearance_user.rb
@@ -8,13 +8,13 @@ module Bugsnag::Middleware
 
     def call(report)
       if report.request_data[:rack_env] &&
-        report.request_data[:rack_env]["clearance"] &&
-        report.request_data[:rack_env]["clearance"].signed_in? &&
-        report.request_data[:rack_env]["clearance"].current_user
+        report.request_data[:rack_env][:clearance] &&
+        report.request_data[:rack_env][:clearance].signed_in? &&
+        report.request_data[:rack_env][:clearance].current_user
 
         # Extract useful user information
         user = {}
-        user_object = report.request_data[:rack_env]["clearance"].current_user
+        user_object = report.request_data[:rack_env][:clearance].current_user
         if user_object
           # Build the bugsnag user info from the current user record
           COMMON_USER_FIELDS.each do |field|
