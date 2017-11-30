6dbe1b5932e7777e7edf124e8f264e502ac3a547
Alex Moinet <alex@bugsnag.com>
2017-11-30 16:47:01 +0000

    Updated tests

--- spec/session_tracker_spec.rb
+++ spec/session_tracker_spec.rb
@@ -1,16 +1,32 @@
 # encoding: utf-8
+require 'webrick'
 require 'spec_helper'
+require 'json'
 
 describe Bugsnag::SessionTracker do
+  server = nil
+  queue = Queue.new
+
   before do
     @config = Bugsnag::Configuration.new
     @config.track_sessions = true
+    server = WEBrick::HTTPServer.new :Port => 0, :Logger => WEBrick::Log.new("/dev/null"), :AccessLog => []
+    server.mount_proc '/' do |req, res|
+      headers = []
+      req.each { |header| headers << header }
+      queue << [JSON.parse(req.body), headers]
+      res.status = 202
+      res.body = "OK\n"
+    end
+    Thread.new{ server.start }
   end
 
-  after do
-    Bugsnag.configure do |conf|
+  after do    Bugsnag.configure do |conf|
       conf.track_sessions = false
+      conf.delivery_method = :synchronous
     end
+    server.stop
+    queue.clear
   end
 
   it 'does not create session object if disabled' do
@@ -68,50 +84,60 @@ describe Bugsnag::SessionTracker do
   it 'sends sessions when send_sessions is called' do
     Bugsnag.configure do |conf|
       conf.track_sessions = true
+      conf.delivery_method = :thread_queue
+      conf.session_endpoint = "http://localhost:#{server.config[:Port]}"
     end
+    WebMock.allow_net_connect!
     Bugsnag.session_tracker.create_session
     expect(Bugsnag.session_tracker.delivery_queue.size).to eq(1)
     Bugsnag.session_tracker.send_sessions
     expect(Bugsnag.session_tracker.delivery_queue.size).to eq(0)
-    expect(Bugsnag).to have_sent_sessions{ |payload, headers|
-      expect(payload.include?("app")).to be true
-      expect(payload.include?("notifier")).to be true
-      expect(payload.include?("device")).to be true
-      expect(payload.include?("sessions")).to be true
-      expect(payload["sessions"].size).to eq(1)
-    }
+    while queue.empty?
+      sleep(0.05)
+    end
+    payload, headers = queue.pop
+    expect(payload.include?("app")).to be true
+    expect(payload.include?("notifier")).to be true
+    expect(payload.include?("device")).to be true
+    expect(payload.include?("sessions")).to be true
+    expect(payload["sessions"].size).to eq(1)
   end
 
   it 'sets details from config' do
     Bugsnag.configure do |conf|
       conf.track_sessions = true
       conf.release_stage = "test_stage"
+      conf.delivery_method = :thread_queue
+      conf.session_endpoint = "http://localhost:#{server.config[:Port]}"
     end
+    WebMock.allow_net_connect!
     Bugsnag.session_tracker.create_session
     expect(Bugsnag.session_tracker.delivery_queue.size).to eq(1)
     Bugsnag.session_tracker.send_sessions
     expect(Bugsnag.session_tracker.delivery_queue.size).to eq(0)
-    expect(Bugsnag).to have_sent_sessions{ |payload, headers|
-      notifier = payload["notifier"]
-      expect(notifier.include?("name")).to be true
-      expect(notifier["name"]).to eq(Bugsnag::Report::NOTIFIER_NAME)
-      expect(notifier.include?("url")).to be true
-      expect(notifier["url"]).to eq(Bugsnag::Report::NOTIFIER_URL)
-      expect(notifier.include?("version")).to be true
-      expect(notifier["version"]).to eq(Bugsnag::Report::NOTIFIER_VERSION)
+    while queue.empty?
+      sleep(0.05)
+    end
+    payload, headers = queue.pop
+    notifier = payload["notifier"]
+    expect(notifier.include?("name")).to be true
+    expect(notifier["name"]).to eq(Bugsnag::Report::NOTIFIER_NAME)
+    expect(notifier.include?("url")).to be true
+    expect(notifier["url"]).to eq(Bugsnag::Report::NOTIFIER_URL)
+    expect(notifier.include?("version")).to be true
+    expect(notifier["version"]).to eq(Bugsnag::Report::NOTIFIER_VERSION)
 
-      app = payload["app"]
-      expect(app.include?("releaseStage")).to be true
-      expect(app["releaseStage"]).to eq(Bugsnag.configuration.release_stage)
-      expect(app.include?("version")).to be true
-      expect(app["version"]).to eq(Bugsnag.configuration.app_version)
-      expect(app.include?("type")).to be true
-      expect(app["type"]).to eq(Bugsnag.configuration.app_type)
+    app = payload["app"]
+    expect(app.include?("releaseStage")).to be true
+    expect(app["releaseStage"]).to eq(Bugsnag.configuration.release_stage)
+    expect(app.include?("version")).to be true
+    expect(app["version"]).to eq(Bugsnag.configuration.app_version)
+    expect(app.include?("type")).to be true
+    expect(app["type"]).to eq(Bugsnag.configuration.app_type)
 
-      device = payload["device"]
-      expect(device.include?("hostname")).to be true
-      expect(device["hostname"]).to eq(Bugsnag.configuration.hostname)
-    }
+    device = payload["device"]
+    expect(device.include?("hostname")).to be true
+    expect(device["hostname"]).to eq(Bugsnag.configuration.hostname)
   end
 
   it 'uses middleware to attach session to notification' do
