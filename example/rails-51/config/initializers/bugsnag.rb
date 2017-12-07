7dbdffaf74a7d3f7504e339d4b817d8e564ac70c
Alex Moinet <alex@bugsnag.com>
2017-12-07 09:51:41 +0000

    Added session-tracking to rails-51 example

--- example/rails-51/config/initializers/bugsnag.rb
+++ example/rails-51/config/initializers/bugsnag.rb
@@ -1,3 +1,6 @@
 Bugsnag.configure do |config|
-  config.api_key = "YOUR_API_KEY_HERE"
+  config.api_key = "f484c54fcee567693cb8cab08ced6a80"
+  config.endpoint = "http://localhost:8000"
+  config.track_sessions = true
+  config.session_endpoint = "http://localhost:10000"
 end
