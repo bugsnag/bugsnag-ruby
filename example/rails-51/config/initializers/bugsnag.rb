c91d3810847aceee911516b40cc484d025237b12
Alex Moinet <alex@bugsnag.com>
2017-12-07 14:38:07 +0000

    Removed unnecessary info

--- example/rails-51/config/initializers/bugsnag.rb
+++ example/rails-51/config/initializers/bugsnag.rb
@@ -1,6 +1,4 @@
 Bugsnag.configure do |config|
-  config.api_key = "f484c54fcee567693cb8cab08ced6a80"
-  config.endpoint = "http://localhost:8000"
+  config.api_key = "YOUR_API_KEY"
   config.track_sessions = true
-  config.session_endpoint = "http://localhost:10000"
 end
