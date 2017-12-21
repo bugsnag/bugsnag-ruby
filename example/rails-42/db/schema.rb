e6d89e03b0cbf961c10372a770e45cb19bea6c70
Alex Moinet <alex@bugsnag.com>
2017-12-21 11:10:55 +0000

    Indentation

--- example/rails-42/db/schema.rb
+++ example/rails-42/db/schema.rb
@@ -13,21 +13,20 @@
 
 ActiveRecord::Schema.define(version: 20171006132129) do
   
-    create_table "delayed_jobs", force: :cascade do |t|
-      t.integer  "priority",   default: 0, null: false
-      t.integer  "attempts",   default: 0, null: false
-      t.text     "handler",                null: false
-      t.text     "last_error"
-      t.datetime "run_at"
-      t.datetime "locked_at"
-      t.datetime "failed_at"
-      t.string   "locked_by"
-      t.string   "queue"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-    end
-  
-    add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
-  
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-  
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+
+end
