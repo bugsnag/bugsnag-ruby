ca1c750b531107d6992ca7a6ae0b2afd5c549e1b
Delisa Mason <iskanamagus@gmail.com>
2018-02-06 14:08:30 -0800

    Format code block as markdown

--- lib/bugsnag/delivery.rb
+++ lib/bugsnag/delivery.rb
@@ -5,11 +5,13 @@ module Bugsnag
       # Add a delivery method to the list of supported methods. Any registered
       # method can then be used by name in Configuration.
       #
+      # ```
       # require 'bugsnag'
       # Bugsnag::Delivery.register(:my_delivery_queue, MyDeliveryQueue)
       # Bugsnag.configure do |config|
       #   config.delivery_method = :my_delivery_queue
       # end
+      # ```
       def register(name, delivery_method)
         delivery_methods[name.to_sym] = delivery_method
       end
