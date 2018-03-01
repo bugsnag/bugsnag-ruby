ce30e05eee3f72a7fd9f18deb03f26dc3f3e048d
Alex Moinet <alex@bugsnag.com>
2018-03-01 13:42:59 +0000

    Ensured que is loaded if Que integration is loaded

--- lib/bugsnag/integrations/que.rb
+++ lib/bugsnag/integrations/que.rb
@@ -1,44 +1,50 @@
-if defined?(::Que)
-  handler = proc do |error, job|
-    begin
-      job = job.dup # Make sure the original job object is not mutated.
+require 'que'
 
-      Bugsnag.notify(error, true) do |report|
-        job[:error_count] += 1
+module Bugsnag
+  module Que
+    def self.handler
+      proc do |error, job|
+        begin
+          job = job.dup # Make sure the original job object is not mutated.
 
-        # If the job was scheduled using ActiveJob then unwrap the job details for clarity:
-        if job[:job_class] == "ActiveJob::QueueAdapters::QueAdapter::JobWrapper"
-          wrapped_job = job[:args].last
-          wrapped_job = wrapped_job.each_with_object({}) { |(k, v), result| result[k.to_sym] = v } # Symbolize keys
+          Bugsnag.notify(error, true) do |report|
+            job[:error_count] += 1
 
-          # Align key names with keys in `job`
-          wrapped_job[:queue] = wrapped_job.delete(:queue_name)
-          wrapped_job[:args]  = wrapped_job.delete(:arguments)
+            # If the job was scheduled using ActiveJob then unwrap the job details for clarity:
+            if job[:job_class] == "ActiveJob::QueueAdapters::QueAdapter::JobWrapper"
+              wrapped_job = job[:args].last
+              wrapped_job = wrapped_job.each_with_object({}) { |(k, v), result| result[k.to_sym] = v } # Symbolize keys
 
-          job.merge!(wrapper_job_class: job[:job_class], wrapper_job_id: job[:job_id]).merge!(wrapped_job)
+              # Align key names with keys in `job`
+              wrapped_job[:queue] = wrapped_job.delete(:queue_name)
+              wrapped_job[:args]  = wrapped_job.delete(:arguments)
+
+              job.merge!(wrapper_job_class: job[:job_class], wrapper_job_id: job[:job_id]).merge!(wrapped_job)
+            end
+
+            report.add_tab(:job, job)
+            report.severity = 'error'
+            report.severity_reason = {
+              :type => Bugsnag::Report::UNHANDLED_EXCEPTION_MIDDLEWARE,
+              :attributes => {
+                :framework => 'Que'
+              }
+            }
+          end
+        rescue => e
+          # Que supresses errors raised by its error handler to avoid killing the worker. Log them somewhere:
+          Bugsnag.configuration.warn("Failed to notify Bugsnag of error in Que job (#{e.class}): #{e.message} \n#{e.backtrace[0..9].join("\n")}")
+          raise
         end
-
-        report.add_tab(:job, job)
-        report.severity = 'error'
-        report.severity_reason = {
-          :type => Bugsnag::Report::UNHANDLED_EXCEPTION_MIDDLEWARE,
-          :attributes => {
-            :framework => 'Que'
-          }
-        }
       end
-    rescue => e
-      # Que supresses errors raised by its error handler to avoid killing the worker. Log them somewhere:
-      Bugsnag.configuration.warn("Failed to notify Bugsnag of error in Que job (#{e.class}): #{e.message} \n#{e.backtrace[0..9].join("\n")}")
-      raise
     end
   end
+end
 
-  if Que.respond_to?(:error_notifier=)
-    Bugsnag.configuration.app_type ||= "que"
-    Que.error_notifier = handler
-  elsif Que.respond_to?(:error_handler=)
-    Bugsnag.configuration.app_type ||= "que"
-    Que.error_handler = handler
-  end
+if ::Que.respond_to?(:error_notifier=)
+  Bugsnag.configuration.app_type ||= "que"
+  ::Que.error_notifier = Bugsnag::Que.handler
+elsif ::Que.respond_to?(:error_handler=)
+  Bugsnag.configuration.app_type ||= "que"
+  ::Que.error_handler = Bugsnag::Que.handler
 end
