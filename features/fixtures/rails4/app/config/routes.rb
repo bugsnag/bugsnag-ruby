83cd448403d0f7c5629a7be1b2d5e818f50a5fd4
Alex Moinet <alex@bugsnag.com>
2018-04-03 14:06:27 +0100

    tests(rails): Add rails Maze tests - Added maze tests for before_notify feature - Removed defunct application_controller

--- features/fixtures/rails4/app/config/routes.rb
+++ features/fixtures/rails4/app/config/routes.rb
@@ -1,4 +1,5 @@
 App::Application.routes.draw do
   get "/unhandled/(:action)", controller: 'unhandled'
   get "/handled/(:action)", controller: 'handled'
+  get "/before_notify/(:action)", controller: 'before_notify'
 end
--- features/fixtures/rails5/app/config/routes.rb
+++ features/fixtures/rails5/app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/unhandled/(:action)", controller: 'unhandled'
   get "/handled/(:action)", controller: 'handled'
+  get "/before_notify/(:action)", controller: 'before_notify'
 end
