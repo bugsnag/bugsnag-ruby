5e956807734b6c74b748b40b72853495bce1d198
Alex Moinet <alex@bugsnag.com>
2018-04-17 17:50:52 +0100

    Further notifications

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,7 @@ require 'open3'
 require 'pp'
 
 def current_ip
+  pp `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*'`
   ip_addr = `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*' | grep -v '127.0.0.1' | awk '{ print $2 }'`.strip
   pp ip_addr
   ip_addr
