89eaf61ee5f0cc17d7a9ac8c9c66e3c6bdefa6a5
Alex Moinet <alex@bugsnag.com>
2018-04-17 17:45:02 +0100

    Testing IP

--- features/steps/ruby_notifier_steps.rb
+++ features/steps/ruby_notifier_steps.rb
@@ -3,7 +3,7 @@ require 'open3'
 
 When("I configure the bugsnag endpoint") do
   steps %Q{
-    When I set environment variable "MAZE_ENDPOINT" to "http://host.docker.internal:#{MOCK_API_PORT}"
+    When I set environment variable "MAZE_ENDPOINT" to "http://#{current_ip}:#{MOCK_API_PORT}"
   }
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,8 @@
 require 'open3'
+require 'pp'
 
 def current_ip
-  `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*' | grep -Eo '([0-9]*\\\.){3}[0-9]*' | grep -v '127.0.0.1'`.strip
+  ip_addr = `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*' | grep -v '127.0.0.1' | awk '{ print $2 }'`.strip
+  pp ip_addr
+  ip_addr
 end
