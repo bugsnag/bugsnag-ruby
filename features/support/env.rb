7b76e5e5cd1ed19974bf28658b08f205fbe7b8f3
Alex Moinet <alex@bugsnag.com>
2018-04-17 18:01:07 +0100

    Use ruby magic TM

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,10 @@ require 'open3'
 require 'pp'
 
 def current_ip
-  pp `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*'`
-  ip_addr = `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*' | grep -v '127.0.0.1' | awk '{ print $2 }'`.strip
+  ip_addr = `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*' | grep -v '127.0.0.1'`
   pp ip_addr
-  ip_addr
+  ip_list = /(([0-9]*\.){3}[0-9]*)/.match(ip_addr)
+  pp ip_list
+  pp ip_list.captures.first
+  ip_list.captures.first
 end
