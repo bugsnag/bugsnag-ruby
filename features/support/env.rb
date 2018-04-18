9364f01bbc32d62428832c01f832e2c53b733326
Alex Moinet <alex@bugsnag.com>
2018-04-18 10:41:51 +0100

    Added info on 'current_ip' function and newlines

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,8 @@
 require 'open3'
 
 def current_ip
+  # Parses the output of `ifconfig` to retreive the host IP for docker to talk to
+  # Breaks compatability with Windows
   ip_addr = `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*' | grep -v '127.0.0.1'`
   ip_list = /((?:[0-9]*\.){3}[0-9]*)/.match(ip_addr)
   ip_list.captures.first
