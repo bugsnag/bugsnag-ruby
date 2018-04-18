48c2abe14eb149ef9ce7ff6472e7d8cfc7ab0008
Alex Moinet <alex@bugsnag.com>
2018-04-18 08:19:51 +0100

    Remove pp commands

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,7 @@
 require 'open3'
-require 'pp'
 
 def current_ip
   ip_addr = `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*' | grep -v '127.0.0.1'`
-  pp ip_addr
   ip_list = /(([0-9]*\.){3}[0-9]*)/.match(ip_addr)
-  pp ip_list
-  pp ip_list.captures.first
   ip_list.captures.first
 end
