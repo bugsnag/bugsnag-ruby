2aaff3ea6f1818ad3e2a2ef5761125a5cb7fee86
Alex Moinet <alex@bugsnag.com>
2018-04-20 15:05:16 +0100

    tests(Rails): Add Rails maze tests - Add default environment variables for non-rails features

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,11 @@
 require 'open3'
 
+def setup_default_environment
+  ENV['RAILS_3_PORT'] = '3000'
+  ENV['RAILS_4_PORT'] = '3000'
+  ENV['RAILS_5_PORT'] = '3000'
+end
+
 def current_ip
   # Parses the output of `ifconfig` to retreive the host IP for docker to talk to
   # Breaks compatability with Windows
@@ -7,3 +13,5 @@ def current_ip
   ip_list = /((?:[0-9]*\.){3}[0-9]*)/.match(ip_addr)
   ip_list.captures.first
 end
+
+setup_default_environment
