16651fc1d09501f24aa5876be33b2a4a3bee010a
Alex Moinet <alex@bugsnag.com>
2018-04-27 15:48:26 +0100

    tests(Integration): Add maze tests - Add output_logs functionality and move `find_default_docker_compose` into the local env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,22 @@
 require 'open3'
 
+Before do
+  find_default_docker_compose
+end
+
+def output_logs
+  $docker_services.each do |service|
+    logged_service = service[:service] == :all ? '' : service[:service]
+    command = "logs -t #{logged_service}"
+    begin
+      response = run_docker_compose_command(service[:file], command)
+    rescue => exception
+      response = "Couldn't retreive logs for #{service[:file]}:#{logged_service}"
+    end
+    STDOUT.puts response.is_a?(String) ? response : response.to_a
+  end
+end
+
 def current_ip
   # Parses the output of `ifconfig` to retreive the host IP for docker to talk to
   # Breaks compatability with Windows
