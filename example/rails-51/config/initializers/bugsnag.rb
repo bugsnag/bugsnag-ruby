97e2e5a9d5db72c10eed2d43ce57a5547d4d32b4
Alex Moinet <alex@bugsnag.com>
2018-06-11 11:50:31 +0100

    fix: Remove local rails config

--- example/rails-51/config/initializers/bugsnag.rb
+++ example/rails-51/config/initializers/bugsnag.rb
@@ -1,5 +1,4 @@
 Bugsnag.configure do |config|
-  config.api_key = "12312312312312312312312312312312"
-  config.auto_capture_sessions = false
-  config.endpoint = "http://localhost:62000"
+  config.api_key = "YOUR_API_KEY_HERE"
+  config.auto_capture_sessions = true
 end
