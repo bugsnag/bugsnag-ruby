2bde9be6c3bbe7ca21e3c9281f40ef711433b646
Alex Moinet <alex@bugsnag.com>
2018-11-02 11:55:01 +0000

    DelayedJob tests: Added two further delayed job scenarios for current integration

--- features/fixtures/delayed_job/app/app/models/test_model.rb
+++ features/fixtures/delayed_job/app/app/models/test_model.rb
@@ -10,4 +10,8 @@ class TestModel < ApplicationRecord
   def self.fail_with_args(a)
     raise "uh oh"
   end
+
+  def self.notify
+    Bugsnag.notify(RuntimeError.new("Handled exception"))
+  end
 end
