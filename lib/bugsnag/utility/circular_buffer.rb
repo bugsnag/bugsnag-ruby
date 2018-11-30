ebd6abce6e37ad058b19981824714355c99f5afb
Alex Moinet <alex@bugsnag.com>
2018-11-30 16:06:03 +0000

    Breadcrumbs/Circular buffer: Added circular buffer class

---
+++ lib/bugsnag/utility/circular_buffer.rb
@@ -0,0 +1,34 @@
+module Bugsnag::Utility
+  class CircularBuffer
+    include Enumerable
+
+    attr_reader :max_items
+
+    def initialize(max_items=25)
+      @max_items = max_items
+      @buffer = []
+    end
+
+    def <<(item)
+      @buffer << item
+      trim_buffer
+      self
+    end
+
+    def each(&block)
+      @buffer.each(&block)
+    end
+
+    def max_items=(new_max_items)
+      @max_items = new_max_items
+      trim_buffer
+    end
+
+    private
+    def trim_buffer
+        trim_size = @buffer.size - @max_items
+        trim_size = 0 if trim_size < 0
+        @buffer.shift(trim_size)
+    end
+  end
+end
