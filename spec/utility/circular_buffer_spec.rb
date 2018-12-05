6f4209f818f7dec791aaece6fbafb1e02b859e3f
Alex Moinet <alex@bugsnag.com>
2018-12-05 15:28:18 +0000

    Breadcrumbs/Cricular buffer: Improve test readability

--- spec/utility/circular_buffer_spec.rb
+++ spec/utility/circular_buffer_spec.rb
@@ -3,49 +3,38 @@
 require 'spec_helper'
 require 'bugsnag/utility/circular_buffer'
 
-describe Bugsnag::Utility::CircularBuffer do
-  describe "buffer size" do
-    it "should be 25 by default" do
+Rspec.describe Bugsnag::Utility::CircularBuffer do
+  describe "#initialize" do
+    it "contains no items" do
+      buffer = Bugsnag::Utility::CircularBuffer.new
+
+      expect(buffer.to_a).to eq([])
+    end
+  end
+
+  describe "#max_items" do
+    it "defaults to 25" do
       buffer = Bugsnag::Utility::CircularBuffer.new
 
       expect(buffer.max_items).to equal 25
     end
 
-    it "can be set during initialization" do
+    it "can be set during #initialize" do
       buffer = Bugsnag::Utility::CircularBuffer.new(10)
 
       expect(buffer.max_items).to equal 10
     end
+  end
 
-    it "can be changed using max_items =" do
+  describe "#max_items=" do
+    it "changes #max_items" do
       buffer = Bugsnag::Utility::CircularBuffer.new(10)
       buffer.max_items = 17
 
       expect(buffer.max_items).to equal(17)
     end
-  end
 
-  describe "buffer items" do
-    it "initializes empty" do
-      buffer = Bugsnag::Utility::CircularBuffer.new
-
-      expect(buffer.to_a).to eq([])
-    end
-
-    it "can be added to with <<" do
-      buffer = Bugsnag::Utility::CircularBuffer.new
-      buffer << 1
-      expect(buffer.to_a).to eq([1])
-    end
-
-    it "are shifted when max_items are exceeded" do
-      buffer = Bugsnag::Utility::CircularBuffer.new(5)
-      (0...10).each { |x| buffer << x }
-
-      expect(buffer.to_a).to eq([5, 6, 7, 8, 9])
-    end
-
-    it "are removed if max_items is reduced" do
+    it "shifts any excess items when reduced" do
       buffer = Bugsnag::Utility::CircularBuffer.new(10)
       (0...10).each { |x| buffer << x }
       buffer.max_items = 3
@@ -53,28 +42,7 @@ describe Bugsnag::Utility::CircularBuffer do
       expect(buffer.to_a).to eq([7, 8, 9])
     end
 
-    it "can be inserted in chains" do
-      buffer = Bugsnag::Utility::CircularBuffer.new
-      buffer << 1 << 2 << 3 << 4 << 5
-
-      expect(buffer.to_a).to eq([1, 2, 3, 4, 5])
-    end
-
-    it "can be iterated over" do
-      buffer = Bugsnag::Utility::CircularBuffer.new
-      buffer << 1
-      buffer << 2
-      buffer << 3
-
-      output = []
-      buffer.each do |x|
-        output << x
-      end
-
-      expect(output).to eq([1, 2, 3])
-    end
-
-    it "respects max_items when increased" do
+    it "increases the maximum capacity" do
       buffer = Bugsnag::Utility::CircularBuffer.new(3)
       buffer << 1
       buffer << 2
@@ -87,6 +55,44 @@ describe Bugsnag::Utility::CircularBuffer do
       buffer << 5
 
       expect(buffer.to_a).to eq([1, 2, 3, 4, 5])
+    end
+  end
+
+  describe "#<<" do
+    it "adds items to the buffer" do
+      buffer = Bugsnag::Utility::CircularBuffer.new
+      buffer << 1
+      expect(buffer.to_a).to eq([1])
+    end
+
+    it "shifts items it #max_items is exceeded" do
+      buffer = Bugsnag::Utility::CircularBuffer.new(5)
+      (0...10).each { |x| buffer << x }
+
+      expect(buffer.to_a).to eq([5, 6, 7, 8, 9])
+    end
+
+    it "can be chained" do
+      buffer = Bugsnag::Utility::CircularBuffer.new
+      buffer << 1 << 2 << 3 << 4 << 5
+
+      expect(buffer.to_a).to eq([1, 2, 3, 4, 5])
+    end
+  end
+
+  describe "#each" do
+    it "can be iterated over" do
+      buffer = Bugsnag::Utility::CircularBuffer.new
+      buffer << 1
+      buffer << 2
+      buffer << 3
+
+      output = []
+      buffer.each do |x|
+        output << x
+      end
+
+      expect(output).to eq([1, 2, 3])
     end
   end
 end
