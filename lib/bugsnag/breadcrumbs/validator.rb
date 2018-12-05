0ac3516dbfc10d4727bff174c61cf60ad3e124f3
Alex Moinet <alex@bugsnag.com>
2018-12-05 17:58:34 +0000

    Breadcrumbs/Validator: Rubocop fixes

--- lib/bugsnag/breadcrumbs/validator.rb
+++ lib/bugsnag/breadcrumbs/validator.rb
@@ -9,25 +9,25 @@ module Bugsnag::Breadcrumbs
     ##
     # Validates a given breadcrumb.
     def validate(breadcrumb)
-      #Check message length
+      # Check message length
       if breadcrumb.message.size > MAX_MESSAGE_LENGTH
         @configuration.warn("Breadcrumb message trimmed to length #{MAX_MESSAGE_LENGTH}.  Original message: #{breadcrumb.message}")
         breadcrumb.message.slice!(MAX_MESSAGE_LENGTH...breadcrumb.message.size)
       end
 
-      #Check meta_data hash doesn't contain complex values
+      # Check meta_data hash doesn't contain complex values
       unless valid_meta_data_types?(breadcrumb.meta_data)
-        @configuration.warn("Breadcrumb #{breadcrumb.message} meta data contains values other than strings, numbers, or booleans, dropping: #{breadcrumb.meta_data}")
+        @configuration.warn("Breadcrumb #{breadcrumb.message} meta_data has values other than strings, numbers, or booleans, dropping: #{breadcrumb.meta_data}")
         breadcrumb.meta_data = {}
       end
 
-      #Check type is valid, set to manual otherwise
+      # Check type is valid, set to manual otherwise
       unless VALID_BREADCRUMB_TYPES.include?(breadcrumb.type)
         @configuration.warn("Invalid type: #{breadcrumb.type} for breadcrumb: #{breadcrumb.message}, defaulting to #{MANUAL_BREADCRUMB_TYPE}")
         breadcrumb.type = MANUAL_BREADCRUMB_TYPE
       end
 
-      #If auto is true, check type is in automatic_breadcrumb_types
+      # If auto is true, check type is in automatic_breadcrumb_types
       if breadcrumb.auto && !@configuration.automatic_breadcrumb_types.include?(breadcrumb.type)
         @configuration.warn("Automatic breadcrumb of type #{breadcrumb.type} ignored: #{breadcrumb.message}")
         breadcrumb.ignore!
@@ -41,7 +41,7 @@ module Bugsnag::Breadcrumbs
     #
     # Acceptable types are String, Numeric, TrueClass, and FalseClass.
     def valid_meta_data_types?(meta_data)
-      meta_data.all? { |k, value| value.is_a?(String) || value.is_a?(Numeric) || value.is_a?(FalseClass) || value.is_a?(TrueClass) }
+      meta_data.all? { |_, value| value.is_a?(String) || value.is_a?(Numeric) || value.is_a?(FalseClass) || value.is_a?(TrueClass) }
     end
   end
 end
