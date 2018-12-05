f602765391ecd43ba9f16b1eefccf1625b1095f5
Alex Moinet <alex@bugsnag.com>
2018-12-05 16:22:31 +0000

    Breadcrumbs/Breadcrumb: Added doc comments

--- lib/bugsnag/breadcrumbs/breadcrumb.rb
+++ lib/bugsnag/breadcrumbs/breadcrumb.rb
@@ -6,23 +6,45 @@ module Bugsnag::Breadcrumbs
     attr_reader :auto
     attr_reader :timestamp
 
+    ##
+    # Creates a breadcrumb.
+    #
+    # This will not have been validated, which must occur before this is
+    # attached to a report.
     def initialize(message, type, meta_data, auto)
       @should_ignore = false
       self.message = message
       self.type = type
       self.meta_data = meta_data
+
+      # Use the symbol comparison to improve readability of breadcrumb creation
       @auto = auto == :auto
+
+      # Store it as a timestamp for now
       @timestamp = Time.now
     end
 
+    ##
+    # Flags the breadcrumb to be ignored.
+    #
+    # Ignored breadcrumbs will not be attached to a report.
     def ignore!
       @should_ignore = true
     end
 
+    ##
+    # Checks if the `ignore!` method has been called.
+    #
+    # Ignored breadcrumbs will not be attached to a report.
     def ignore?
       @should_ignore
     end
 
+    ##
+    # Outputs the breadcrumb data in a formatted hash.
+    #
+    # These adhere to the breadcrumb format as defined in the Bugsnag error 
+    # reporting API
     def to_h
       {
         :name => @message,
