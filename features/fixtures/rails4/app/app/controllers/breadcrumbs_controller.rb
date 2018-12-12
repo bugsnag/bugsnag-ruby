82dcd4842c5de0c07497dd5d1c0f39d8039de298
Alex Moinet <alex@bugsnag.com>
2018-12-12 14:53:11 +0000

    Breadcrumbs/Rails: Add initial maze tests

---
+++ features/fixtures/rails4/app/app/controllers/breadcrumbs_controller.rb
@@ -0,0 +1,18 @@
+class BreadcrumbsController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def index
+    render json: {}
+  end
+
+  def handled
+    Bugsnag.notify("Request breadcrumb")
+    render json: {}
+  end
+
+  def sql_breadcrumb
+    User.take
+    Bugsnag.notify("SQL breadcrumb")
+    render json: {}
+  end
+end
