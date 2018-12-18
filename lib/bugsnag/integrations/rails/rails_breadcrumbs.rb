df058704c488e4eae95e1f3c9860b2af4d2ab23b
Alex Moinet <alex@bugsnag.com>
2018-12-18 17:25:44 +0000

    Breadcrumbs/Rails breadcrumbs: Ammended active_job event id

--- lib/bugsnag/integrations/rails/rails_breadcrumbs.rb
+++ lib/bugsnag/integrations/rails/rails_breadcrumbs.rb
@@ -12,8 +12,8 @@ module Bugsnag::Rails
       ]
     },
     {
-      :id => "perform.active_job",
-      :message => "Perform ActiveJob",
+      :id => "perform_start.active_job",
+      :message => "Start perform ActiveJob",
       :type => Bugsnag::Breadcrumbs::PROCESS_BREADCRUMB_TYPE,
       :allowed_data => []
     },
