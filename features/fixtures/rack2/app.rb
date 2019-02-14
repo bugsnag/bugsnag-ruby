e17cfe4c14db6781cd541efb4d00bb1fda6af593
Alex Moinet <alex@bugsnag.com>
2019-02-14 10:47:10 +0000

    Fixtures: Use existing Rack fixtures

--- features/fixtures/rack2/app.rb
+++
@@ -1,8 +0,0 @@
-require 'bundler'
-Bundler.require
-
-app = Proc.new do |env|
-    ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app.']]
-end
-
-Rack::Handler::WEBrick.run app, {Port: 3000, BindAddress: '0.0.0.0'}
