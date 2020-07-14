5d546dfdf8b4ddb81d8ed459744fb5e7af4efab6
Joe Haines <hello@joehaines.co.uk>
2020-07-14 10:27:39 +0100

    Extract a helper to fetch the code from an event
    This also uses 'to_a' to ensure the order of code is correct. Hashes are
    compared without regard for order because usually the order doesn't
    matter. I don't think the API cares about the order because we explicitly
    number the lines, but it's useful to validate our code is doing what is
    expected

--- spec/stacktrace_spec.rb
+++ spec/stacktrace_spec.rb
@@ -15,10 +15,10 @@ describe Bugsnag::Stacktrace do
         Bugsnag.notify(e)
       end
 
-      expect(Bugsnag).to have_sent_notification{ |payload, headers|
-        exception = get_exception_from_payload(payload)
-        starting_line = __LINE__ - 13
-        expect(exception["stacktrace"][0]["code"]).to eq({
+      expect(Bugsnag).to have_sent_notification { |payload, headers|
+        starting_line = __LINE__ - 12
+
+        expect(get_code_from_payload(payload).to_a).to eq({
           (starting_line + 0).to_s => '        _a = 1',
           (starting_line + 1).to_s => '        _b = 2',
           (starting_line + 2).to_s => '        _c = 3',
@@ -26,7 +26,7 @@ describe Bugsnag::Stacktrace do
           (starting_line + 4).to_s => '        _d = 4',
           (starting_line + 5).to_s => '        _e = 5',
           (starting_line + 6).to_s => '        _f = 6'
-          })
+        }.to_a)
       }
     end
 
@@ -35,19 +35,16 @@ describe Bugsnag::Stacktrace do
 
       notify_test_exception
 
-      expect(Bugsnag).to have_sent_notification{ |payload, headers|
-        exception = get_exception_from_payload(payload)
-        expect(exception["stacktrace"][1]["code"]).to eq(nil)
+      expect(Bugsnag).to have_sent_notification { |payload, headers|
+        expect(get_code_from_payload(payload)).to eq(nil)
       }
     end
 
     it 'should send the first 7 lines of the file for exceptions near the top' do
       load 'spec/fixtures/crashes/start_of_file.rb' rescue Bugsnag.notify $!
 
-      expect(Bugsnag).to have_sent_notification{ |payload, headers|
-        exception = get_exception_from_payload(payload)
-
-        expect(exception["stacktrace"][0]["code"]).to eq({
+      expect(Bugsnag).to have_sent_notification { |payload, headers|
+        expect(get_code_from_payload(payload).to_a).to eq({
           "1" => "#",
           "2" => "raise 'hell'",
           "3" => "#",
@@ -55,17 +52,15 @@ describe Bugsnag::Stacktrace do
           "5" => "#",
           "6" => "#",
           "7" => "#"
-        })
+        }.to_a)
       }
     end
 
     it 'should send the last 7 lines of the file for exceptions near the bottom' do
       load 'spec/fixtures/crashes/end_of_file.rb' rescue Bugsnag.notify $!
 
-      expect(Bugsnag).to have_sent_notification{ |payload, headers|
-        exception = get_exception_from_payload(payload)
-
-        expect(exception["stacktrace"][0]["code"]).to eq({
+      expect(Bugsnag).to have_sent_notification { |payload, headers|
+        expect(get_code_from_payload(payload)).to eq({
           "3" => "#",
           "4" => "#",
           "5" => "#",
@@ -77,15 +72,15 @@ describe Bugsnag::Stacktrace do
       }
     end
 
-    it 'should send the last 7 lines of the file for exceptions near the bottom' do
+    it 'should send every line of a very short file' do
       load 'spec/fixtures/crashes/short_file.rb' rescue Bugsnag.notify $!
 
-      expect(Bugsnag).to have_sent_notification{ |payload, headers|
-        exception = get_exception_from_payload(payload)
-
-        expect(exception["stacktrace"][0]["code"]).to eq({
-          "1" => "raise 'hell'"
-        })
+      expect(Bugsnag).to have_sent_notification { |payload, headers|
+        expect(get_code_from_payload(payload).to_a).to eq({
+          "1" => "#",
+          "2" => "raise 'hell'",
+          "3" => "#"
+        }.to_a)
       }
     end
   end
