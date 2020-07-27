fe9d47b60ec961986cde9841783755847a3de738
Joe Haines <hello@joehaines.co.uk>
2020-07-27 15:30:40 +0100

    Fix Que feature

--- features/fixtures/que/app/app.rb
+++ features/fixtures/que/app/app.rb
@@ -21,6 +21,16 @@ ActiveRecord::Base.establish_connection(
 Que.connection = ActiveRecord
 Que.migrate!(version: 3)
 
+# Workaround a bug in que/pg
+# see https://github.com/que-rb/que/issues/247
+Que::Adapters::Base::CAST_PROCS[1184] = lambda do |value|
+  case value
+  when Time then value
+  when String then Time.parse(value)
+  else raise "Unexpected time class: #{value.class} (#{value.inspect})"
+  end
+end
+
 class UnhandledJob < Que::Job
   def run
     raise RuntimeError.new("Unhandled error")
