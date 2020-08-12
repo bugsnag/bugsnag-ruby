ce59ffc743c70d8cf35bee27fda74123052382a5
Joe Haines <hello@joehaines.co.uk>
2020-08-12 17:56:35 +0100

    Use thread_queue delivery in Sidekiq

--- features/fixtures/sidekiq/app/app.rb
+++ features/fixtures/sidekiq/app/app.rb
@@ -6,6 +6,17 @@ Bugsnag.configure do |conf|
   conf.api_key = ENV['BUGSNAG_API_KEY']
   puts "Configuring `endpoint` to #{ENV['BUGSNAG_ENDPOINT']}"
   conf.endpoint = ENV['BUGSNAG_ENDPOINT']
+
+  if ENV.include?('BUGSNAG_DELIVERY_METHOD')
+    puts "Configuring `delivery_method` to #{ENV['BUGSNAG_DELIVERY_METHOD']}"
+    conf.delivery_method = ENV['BUGSNAG_DELIVERY_METHOD'].to_sym
+  end
+
+  conf.add_on_error(proc do |report|
+    report.add_tab(:config, {
+      delivery_method: conf.delivery_method.to_s
+    })
+  end)
 end
 
 Sidekiq.configure_client do |config|
