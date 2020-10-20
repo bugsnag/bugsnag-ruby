e08fcc9c5a57ed9b686ea3554e5dc72135a2fc20
Joe Haines <hello@joehaines.co.uk>
2020-10-20 15:13:27 +0100

    Add hack to workaround a maze runner bug
    This is temporary to get CI passing, not something we want to keep!

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,7 +15,31 @@ AfterConfiguration do |config|
   install_fixture_gems
 end
 
+class RunnerEnvironment < Hash
+  # Override Hash#clear with a version that keeps the API key and endpoint
+  # These are set before every test anyway, so there's no need to get rid of them
+  def clear
+    self.keep_if do |key, value|
+      key == "BUGSNAG_API_KEY" || key == "BUGSNAG_ENDPOINT"
+    end
+  end
+end
+
 Before do
+  # FIXME: This is a hack to work around a Maze Runner bug!
+  # Maze Runner now clears the environment between tests automatically, but this
+  # happens _after_ this Before block runs. Therefore the API key and endpoint
+  # that we set here are removed, so all of the tests fail because they can't
+  # report anything. Once that issue is resolved, we can remove this and the
+  # RunnerEnvironment class
+  class Runner
+    class << self
+      def environment
+        @env ||= RunnerEnvironment.new
+      end
+    end
+  end
+
   Docker.compose_project_name = "#{rand.to_s}:#{Time.new.strftime("%s")}"
   Runner.environment.clear
   Runner.environment["BUGSNAG_API_KEY"] = $api_key
