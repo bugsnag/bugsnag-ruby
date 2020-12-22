c76098d7c9755a37c5b78afa7fe472d1c9a9bc5c
Joe Haines <hello@joehaines.co.uk>
2020-12-22 14:40:27 +0000

    Simplify validator_spec

--- spec/breadcrumbs/validator_spec.rb
+++ spec/breadcrumbs/validator_spec.rb
@@ -5,100 +5,68 @@ require 'bugsnag/breadcrumbs/breadcrumb'
 require 'bugsnag/breadcrumbs/validator'
 
 RSpec.describe Bugsnag::Breadcrumbs::Validator do
-  let(:enabled_automatic_breadcrumb_types) { Bugsnag::Breadcrumbs::VALID_BREADCRUMB_TYPES }
-  let(:auto) { false }
+  let(:auto) { :manual }
   let(:name) { "Valid message" }
   let(:type) { Bugsnag::Breadcrumbs::ERROR_BREADCRUMB_TYPE }
-  let(:meta_data) { {} }
 
   describe "#validate" do
     it "does not 'ignore!' a valid breadcrumb" do
-      config = instance_double(Bugsnag::Configuration)
-      allow(config).to receive(:enabled_automatic_breadcrumb_types).and_return(enabled_automatic_breadcrumb_types)
-      validator = Bugsnag::Breadcrumbs::Validator.new(config)
+      breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new(name, type, {}, auto)
 
-      breadcrumb = instance_double(Bugsnag::Breadcrumbs::Breadcrumb, {
-        :auto => auto,
-        :name => name,
-        :type => type,
-        :meta_data => meta_data,
-        :meta_data= => nil
-      })
+      expect(breadcrumb.ignore?).to eq(false)
 
-      expect(breadcrumb).to_not receive(:ignore!)
-      expect(config).to_not receive(:debug)
-
+      validator = Bugsnag::Breadcrumbs::Validator.new(Bugsnag.configuration)
       validator.validate(breadcrumb)
+
+      expect(breadcrumb.ignore?).to eq(false)
     end
 
     it "tests type, defaulting to 'manual' if invalid" do
-      config = instance_double(Bugsnag::Configuration)
-      allow(config).to receive(:enabled_automatic_breadcrumb_types).and_return(enabled_automatic_breadcrumb_types)
-      validator = Bugsnag::Breadcrumbs::Validator.new(config)
+      invalid_type = "I'm not a valid type :-)"
+      breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new(name, invalid_type, {}, auto)
 
-      type = "Not a valid type"
+      expect(breadcrumb.ignore?).to eq(false)
+      expect(breadcrumb.type).to eq(invalid_type)
 
-      breadcrumb = instance_double(Bugsnag::Breadcrumbs::Breadcrumb, {
-        :auto => auto,
-        :name => name,
-        :type => type,
-        :meta_data => meta_data,
-        :meta_data= => nil
-      })
-
-      expect(breadcrumb).to receive(:type=).with(Bugsnag::Breadcrumbs::MANUAL_BREADCRUMB_TYPE)
-      expect(breadcrumb).to_not receive(:ignore!)
-      expected_string = "Invalid type: #{type} for breadcrumb: #{breadcrumb.name}, defaulting to #{Bugsnag::Breadcrumbs::MANUAL_BREADCRUMB_TYPE}"
-      expect(config).to receive(:debug).with(expected_string)
-
+      validator = Bugsnag::Breadcrumbs::Validator.new(Bugsnag.configuration)
       validator.validate(breadcrumb)
+
+      expect(breadcrumb.ignore?).to eq(false)
+      expect(breadcrumb.type).to eq(Bugsnag::Breadcrumbs::MANUAL_BREADCRUMB_TYPE)
     end
 
     describe "with enabled_automatic_breadcrumb_types set" do
       it "rejects automatic breadcrumbs with rejected types" do
-        config = instance_double(Bugsnag::Configuration)
-        allowed_breadcrumb_types = []
-        allow(config).to receive(:enabled_automatic_breadcrumb_types).and_return(allowed_breadcrumb_types)
-        validator = Bugsnag::Breadcrumbs::Validator.new(config)
+        Bugsnag.configuration.logger = spy(Logger)
+        Bugsnag.configuration.enabled_automatic_breadcrumb_types.delete(type)
 
-        auto = true
-        type = Bugsnag::Breadcrumbs::ERROR_BREADCRUMB_TYPE
+        breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new(name, type, {}, :auto)
 
-        breadcrumb = instance_double(Bugsnag::Breadcrumbs::Breadcrumb, {
-          :auto => auto,
-          :name => name,
-          :type => type,
-          :meta_data => meta_data,
-          :meta_data= => nil
-        })
+        expect(breadcrumb.ignore?).to eq(false)
 
-        expect(breadcrumb).to receive(:ignore!)
-        expected_string = "Automatic breadcrumb of type #{Bugsnag::Breadcrumbs::ERROR_BREADCRUMB_TYPE} ignored: #{breadcrumb.name}"
-        expect(config).to receive(:debug).with(expected_string)
-
+        validator = Bugsnag::Breadcrumbs::Validator.new(Bugsnag.configuration)
         validator.validate(breadcrumb)
+
+        expect(breadcrumb.ignore?).to eq(true)
+
+        expect(Bugsnag.configuration.logger).to have_received(:debug) do |&block|
+          expect(block.call).to eq("Automatic breadcrumb of type #{type} ignored: #{name}")
+        end
       end
 
       it "does not reject manual breadcrumbs with rejected types" do
-        config = instance_double(Bugsnag::Configuration)
-        allowed_breadcrumb_types = []
-        allow(config).to receive(:enabled_automatic_breadcrumb_types).and_return(allowed_breadcrumb_types)
-        validator = Bugsnag::Breadcrumbs::Validator.new(config)
+        Bugsnag.configuration.logger = spy(Logger)
+        Bugsnag.configuration.enabled_automatic_breadcrumb_types.delete(type)
 
-        type = Bugsnag::Breadcrumbs::ERROR_BREADCRUMB_TYPE
+        breadcrumb = Bugsnag::Breadcrumbs::Breadcrumb.new(name, type, {}, :manual)
 
-        breadcrumb = instance_double(Bugsnag::Breadcrumbs::Breadcrumb, {
-          :auto => auto,
-          :name => name,
-          :type => type,
-          :meta_data => meta_data,
-          :meta_data= => nil
-        })
+        expect(breadcrumb.ignore?).to eq(false)
 
-        expect(breadcrumb).to_not receive(:ignore!)
-        expect(config).to_not receive(:debug)
-
+        validator = Bugsnag::Breadcrumbs::Validator.new(Bugsnag.configuration)
         validator.validate(breadcrumb)
+
+        expect(breadcrumb.ignore?).to eq(false)
+        expect(Bugsnag.configuration.logger).to_not have_received(:debug)
       end
     end
   end
