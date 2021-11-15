708772e7d0ea85ddc4b74bfbe000757b777860ad
Adam Palmblad <adam.palmblad@clio.com>
2021-11-15 11:08:40 -0800

    ADd require

--- spec/integrations/active_record_spec.rb
+++ spec/integrations/active_record_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
-
+require "bugsnag/integrations/rails/active_record_rescue"
 describe Bugsnag::Rails::ActiveRecordRescue do
   it "does not include module in rails 5 and newer" do
-    if Rails::VERSION::MAJOR < 5
+    if ::Rails::VERSION::MAJOR < 5
       skip "test not for this version"
     else
       expect(ActiveRecord::Base.ancestors).not_to include(Bugsnag::Rails::ActiveRecordRescue)
@@ -10,7 +10,7 @@ describe Bugsnag::Rails::ActiveRecordRescue do
   end
 
   it "includes it if the exception behavior is to not raise" do
-    if Rails::VERSION::MAJOR != 4
+    if ::Rails::VERSION::MAJOR != 4
       skip "test not for this version"
     else
       ActiveRecord::Base.errors_in_transactional_callbacks = :log
@@ -19,7 +19,7 @@ describe Bugsnag::Rails::ActiveRecordRescue do
   end
 
   it "does not include it if the exception behavior is to raise" do
-    if Rails::VERSION::MAJOR != 4
+    if ::Rails::VERSION::MAJOR != 4
       skip "test not for this version"
     else
       ActiveRecord::Base.errors_in_transactional_callbacks = :raise
@@ -28,7 +28,7 @@ describe Bugsnag::Rails::ActiveRecordRescue do
   end
 
   it "includes it in rails 3" do
-    if Rails::VERSION::MAJOR != 3
+    if ::Rails::VERSION::MAJOR != 3
       skip "test not for this version"
     else
       expect(ActiveRecord::Base.ancestors).to include(Bugsnag::Rails::ActiveRecordRescue)
