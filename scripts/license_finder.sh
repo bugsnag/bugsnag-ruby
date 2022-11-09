05acfeff5148725e36dc5444a2b7549e48a721c1
Joe Haines <hello@joehaines.co.uk>
2022-11-09 11:35:07 +0000

    Remove unused license finder dockerfile & script

--- scripts/license_finder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl https://raw.githubusercontent.com/bugsnag/license-audit/master/config/decision_files/global.yml -o config/decisions.yml
-curl https://raw.githubusercontent.com/bugsnag/license-audit/master/config/decision_files/bugsnag-ruby.yml >> config/decisions.yml
-
-bundle install
-license_finder --decisions-file=config/decisions.yml
