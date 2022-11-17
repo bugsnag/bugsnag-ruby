748f5567999ac984ffe3b8e5e5becf4da5b3a2ce
Joe Haines <hello@joehaines.co.uk>
2022-11-17 11:30:30 +0000

    Test feature flags in Rails 3

---
+++ features/fixtures/rails3/app/app/controllers/feature_flags_controller.rb
@@ -0,0 +1,27 @@
+class FeatureFlagsController < ActionController::Base
+  protect_from_forgery
+
+  before_bugsnag_notify :add_feature_flags
+
+  def unhandled
+    raise 'oh no'
+  end
+
+  def handled
+    Bugsnag.notify(RuntimeError.new('ahhh'))
+
+    render json: {}
+  end
+
+  private
+
+  def add_feature_flags(event)
+    params['flags'].each do |key, value|
+      event.add_feature_flag(key, value)
+    end
+
+    if params.key?('clear_all_flags')
+      event.clear_feature_flags
+    end
+  end
+end
