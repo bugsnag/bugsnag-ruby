97352473fba1f1dd94b005a1ec060edfbea689bf
Joe Haines <hello@joehaines.co.uk>
2022-11-17 12:45:33 +0000

    Forward feature flags API calls to configuration

--- lib/bugsnag.rb
+++ lib/bugsnag.rb
@@ -429,6 +429,42 @@ module Bugsnag
       configuration.clear_metadata(section, *args)
     end
 
+    # Add a feature flag with the given name & variant
+    #
+    # @param name [String]
+    # @param variant [String, nil]
+    # @return [void]
+    def add_feature_flag(name, variant = nil)
+      configuration.add_feature_flag(name, variant)
+    end
+
+    # Merge the given array of FeatureFlag instances into the stored feature
+    # flags
+    #
+    # New flags will be appended to the array. Flags with the same name will be
+    # overwritten, but their position in the array will not change
+    #
+    # @param feature_flags [Array<Bugsnag::FeatureFlag>]
+    # @return [void]
+    def add_feature_flags(feature_flags)
+      configuration.add_feature_flags(feature_flags)
+    end
+
+    # Remove the stored flag with the given name
+    #
+    # @param name [String]
+    # @return [void]
+    def clear_feature_flag(name)
+      configuration.clear_feature_flag(name)
+    end
+
+    # Remove all the stored flags
+    #
+    # @return [void]
+    def clear_feature_flags
+      configuration.clear_feature_flags
+    end
+
     private
 
     def should_deliver_notification?(exception, auto_notify)
