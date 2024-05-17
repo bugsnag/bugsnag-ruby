5be33d4ca9d392ae9f18303680a5f5f762d6661b
Joe Haines <hello@joehaines.co.uk>
2024-05-17 08:57:24 +0100

    Only set the warden scope if there's a user object
    This prevents having a user object that only contains the scope with no
    other information

--- spec/integrations/warden_user_spec.rb
+++ spec/integrations/warden_user_spec.rb
@@ -78,4 +78,31 @@ describe Bugsnag::Middleware::WardenUser do
     middleware = Bugsnag::Middleware::WardenUser.new(callback)
     middleware.call(report)
   end
+
+  it "doesn't set the user if the user object is empty" do
+    callback = double
+
+    warden = double
+    allow(warden).to receive(:user).with({
+      scope: "user",
+      run_callbacks: false,
+    }).and_return(nil)
+
+    report = double("Bugsnag::Report")
+    expect(report).to receive(:request_data).exactly(3).times.and_return({
+      :rack_env => {
+        "warden" => warden,
+        "rack.session" => {
+          "warden.user.user.key" => "TEST_USER"
+        }
+      }
+    })
+
+    expect(report).not_to receive(:user=)
+
+    expect(callback).to receive(:call).with(report)
+
+    middleware = Bugsnag::Middleware::WardenUser.new(callback)
+    middleware.call(report)
+  end
 end
